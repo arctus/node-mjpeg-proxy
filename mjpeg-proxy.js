8d14c3770db63cc30899e4b0a8274ee99d1d9797
Nadim <nadim@Nadims-iMac.local>
2017-07-24 15:53:57 -0400

    restart connection when camera fails

--- mjpeg-proxy.js
+++ mjpeg-proxy.js
@@ -57,70 +57,74 @@ var MjpegProxy = exports.MjpegProxy = function(mjpegUrl) {
       return;
     }
 
-    // There is already another client consuming the MJPEG response
+    
+    // This is the first client
     if (self.mjpegRequest !== null) {
       self._newClient(req, res);
     } else {
       // Send source MJPEG request
-      self.mjpegRequest = http.request(self.mjpegOptions, function(mjpegResponse) {
-        // console.log('request');
-        self.globalMjpegResponse = mjpegResponse;
-        self.boundary = extractBoundary(mjpegResponse.headers['content-type']);
-
-        self._newClient(req, res);
-
-        var lastByte1 = null;
-        var lastByte2 = null;
-
-        mjpegResponse.on('data', function(chunk) {
-          // Fix CRLF issue on iOS 6+: boundary should be preceded by CRLF.
-          if (lastByte1 != null && lastByte2 != null) {
-            var oldheader = '--' + self.boundary;
-            var p = buffertools.indexOf(chunk, oldheader);
-
-            if (p == 0 && !(lastByte2 == 0x0d && lastByte1 == 0x0a) || p > 1 && !(chunk[p - 2] == 0x0d && chunk[p - 1] == 0x0a)) {
-              var b1 = chunk.slice(0, p);
-              var b2 = new Buffer('\r\n--' + self.boundary);
-              var b3 = chunk.slice(p + oldheader.length);
-              chunk = Buffer.concat([b1, b2, b3]);
-            }
-          }
-
-          lastByte1 = chunk[chunk.length - 1];
-          lastByte2 = chunk[chunk.length - 2];
-
-          for (var i = self.audienceResponses.length; i--;) {
-            var res = self.audienceResponses[i];
-
-            // First time we push data... lets start at a boundary
-            if (self.newAudienceResponses.indexOf(res) >= 0) {
-              var p = buffertools.indexOf(chunk, '--' + self.boundary);
-              if (p >= 0) {
-                res.write(chunk.slice(p));
-                self.newAudienceResponses.splice(self.newAudienceResponses.indexOf(res), 1); // remove from new
-              }
-            } else {
-              res.write(chunk);
-            }
-          }
-        });
-        mjpegResponse.on('end', function () {
-          // console.log("...end");
-          for (var i = self.audienceResponses.length; i--;) {
-            var res = self.audienceResponses[i];
-            res.end();
-          }
-        });
-        mjpegResponse.on('close', function () {
-          // console.log("...close");
-        });
-      });
-
-      self.mjpegRequest.on('error', function(e) {
-        console.error('problem with request: ', e);
-      });
-      self.mjpegRequest.end();
+      self._startRequest(req, res)
     }
+  }
+
+  self._requestResponse = function(mjpegResponse, req, res) {
+    self.globalMjpegResponse = mjpegResponse;
+    self.boundary = extractBoundary(mjpegResponse.headers['content-type']);
+
+    self._newClient(req, res);
+
+    var lastByte1 = null;
+    var lastByte2 = null;
+
+    mjpegResponse.on('data', function(chunk) {
+      // Fix CRLF issue on iOS 6+: boundary should be preceded by CRLF.
+      if (lastByte1 != null && lastByte2 != null) {
+        var oldheader = '--' + self.boundary;
+        var p = buffertools.indexOf(chunk, oldheader);
+
+        if (p == 0 && !(lastByte2 == 0x0d && lastByte1 == 0x0a) || p > 1 && !(chunk[p - 2] == 0x0d && chunk[p - 1] == 0x0a)) {
+          var b1 = chunk.slice(0, p);
+          var b2 = new Buffer('\r\n--' + self.boundary);
+          var b3 = chunk.slice(p + oldheader.length);
+          chunk = Buffer.concat([b1, b2, b3]);
+        }
+      }
+      lastByte1 = chunk[chunk.length - 1];
+      lastByte2 = chunk[chunk.length - 2];
+
+      for (var i = self.audienceResponses.length; i--;) {
+        var res = self.audienceResponses[i];
+
+        // First time we push data... lets start at a boundary
+        if (self.newAudienceResponses.indexOf(res) >= 0) {
+          var p = buffertools.indexOf(chunk, '--' + self.boundary);
+          if (p >= 0) {
+            var test = chunk.slice(p)
+            res.write(test);
+            self.newAudienceResponses.splice(self.newAudienceResponses.indexOf(res), 1); // remove from new
+          }
+        } else {
+          res.write(chunk);
+        }
+      }
+    });
+    
+    mjpegResponse.on('end', function () {
+      console.log("cam cut connection, restarting...");
+      self._startRequest(req, res)
+    });
+
+    mjpegResponse.on('close', function () {
+      console.log("...close");
+    });
+  }
+
+  self._startRequest = function (req, res) {
+    self.mjpegRequest = http.request(self.mjpegOptions, (response) => self._requestResponse(response, req, res));
+    self.mjpegRequest.on('error', function(e) {
+      console.error('problem with request: ', e);
+    });
+    self.mjpegRequest.end();
   }
 
   self._newClient = function(req, res) {
@@ -130,13 +134,10 @@ var MjpegProxy = exports.MjpegProxy = function(mjpegUrl) {
       'Pragma': 'no-cache',
       'Content-Type': 'multipart/x-mixed-replace;boundary=' + self.boundary
     });
-
     self.audienceResponses.push(res);
     self.newAudienceResponses.push(res);
 
     res.socket.on('close', function () {
-      // console.log('exiting client!');
-
       self.audienceResponses.splice(self.audienceResponses.indexOf(res), 1);
       if (self.newAudienceResponses.indexOf(res) >= 0) {
         self.newAudienceResponses.splice(self.newAudienceResponses.indexOf(res), 1); // remove from new
